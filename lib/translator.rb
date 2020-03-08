64245dbbca7cfab4d039fd7f34eabbcd4b4a76a5
Bret Gibson <bret.gibson@gmail.com>
2020-03-08 15:47:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,7 @@ def get_japanese_emoticon(path, english)
   library[:get_emoticon].each do |k, v|
     if english == k
       returnVal = v
+      binding.pry
     else
       returnVal = nil
     end
